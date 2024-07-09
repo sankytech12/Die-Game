4fc31ccbb76f4faa1c49048785f6418c31dc9e51
sankytech12 <sah.sasank12@gmail.com>
2024-07-09 10:57:43 +0530

    Added Main class as entry point

--- Player.java
+++ Player.java
@@ -1,8 +1,8 @@
-class Players{
+class Player{
     private int health;
     private int strength;
     private int attack;
-    Players(int health,int strength,int attack){
+    Player(int health,int strength,int attack){
         this.health=health;
         this.strength=strength;
         this.attack=attack;
