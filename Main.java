89361459ca98f8c15afa9f90f1a0165a11c2be48
sankytech12 <sah.sasank12@gmail.com>
2024-07-09 11:03:42 +0530

    Added Die class for dice rolling

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Player p1=new Player(10, 5, 10);
         System.out.println(p1.isAlive());
+        Die d=new Die();
+        System.out.println(d.roll());
     }
 }
--- Player.java
+++ Player.java
@@ -2,7 +2,7 @@ class Player{
     private int health;
     private int strength;
     private int attack;
-    Player(int health,int strength,int attack){
+    public Player(int health,int strength,int attack){
         this.health=health;
         this.strength=strength;
         this.attack=attack;
